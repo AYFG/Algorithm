abf9f69965812c9f1704b53293749695d40c0159
AYFG <88381607+AYFG@users.noreply.github.com>
2023-12-08 23:29:44 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    let arr = []
+    numbers.sort((a,b)=>a-b)
+    return Math.max((numbers[(numbers.length) -1]) * numbers[(numbers.length-2)],
+        numbers[0] * numbers[1])
+}

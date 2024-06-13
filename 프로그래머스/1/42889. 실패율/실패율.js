9360f795d4b7b57b06e2cd45654378037ad2ec5c
AYFG <88381607+AYFG@users.noreply.github.com>
2024-06-13 20:07:17 +0900

    [level 1] Title: 실패율, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -4,15 +4,15 @@ function solution(N, stages) {
     let 분모;
     let 분자;
     for(let i = 1; i <= N; i++){
-        분모 = stages.filter((stage)=>stage == i).length
-        // console.log(분모)
-        분자 = stages.filter((stage)=>stage >= i).length
-        // console.log(분자)
-        arr.set(i,분모/분자*100)
+        분자 = stages.filter((stage)=>stage == i).length
+        // console.log(`분자${분자}`)
+        분모 = stages.filter((stage)=>stage >= i).length
+        // console.log(`분모${분모}`)
+        arr.set(i,분자/분모*100) 
         // console.log(arr)
     }
     let sortArr = [...arr].sort((a, b) => b[1] - a[1]);
-    // console.log(sortArr)
+    // console.log(`sortArr = ${sortArr}`)
     // console.log(sortArr.length)
     for(let i = 0; i < sortArr.length; i++){
         answer.push(sortArr[i][0])

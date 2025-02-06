e64d86d8ae17fdef5306e6a8f9a88f2b0f5cd3e3
AYFG <sek3137@naver.com>
2025-02-06 20:19:49 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.57 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
 
 ### 성능 요약
 
-메모리: 43.5 MB, 시간: 0.75 ms
+메모리: 43 MB, 시간: 0.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 15:53:09
+2025년 02월 06일 20:19:46
 
 ### 문제 설명
 
--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -3,10 +3,16 @@ function solution(arr) {
     // console.log(arr.indexOf(Math.min(...arr))) // 배열에서 가장 작은 수의 index
     const minNumIndex = arr.indexOf(Math.min(...arr))
     
-    if(arr.length > 1){
+    // if(arr.length > 1){
+    //     arr.splice(minNumIndex,1)
+    //     return arr
+    // }else {
+    //     return [-1]
+    // }
+    if(arr.length == 1){
+        return [-1]
+    }else {
         arr.splice(minNumIndex,1)
         return arr
-    }else {
-        return [-1]
     }
 }

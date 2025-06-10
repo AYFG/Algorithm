51b90b34ff7712784149b8da1cabb0fe1089cd71
AYFG <sek82468246@gmail.com>
2025-06-10 17:30:49 +0900

    재귀

--- masterClass/7_Recursion.js
+++ masterClass/7_Recursion.js
@@ -12,4 +12,28 @@ function countDown(num) {
   num--;
   countDown(num);
 }
-countDown(5);
+// countDown(5);
+
+// second recursive function
+function sumRange(num) {
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
+}
+// sumRange(4);
+
+// for loop factorial
+function factorial(num) {
+  let total = 1;
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
+}
+// console.log(factorial(5));
+
+// recursive factorial
+function recursiveFactorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
+console.log(recursiveFactorial(5));

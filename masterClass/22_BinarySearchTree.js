5d3e8c0739c684c1750e7711f7fe34ff85fd8fd5
AYFG <sek82468246@gmail.com>
2025-06-10 16:33:24 +0900

    BinarySearchTree add Method insert,find,contains

--- masterClass/22_BinarySearchTree.js
+++ masterClass/22_BinarySearchTree.js
@@ -13,13 +13,84 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) {
+          return undefined;
+        }
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+          current = current.left;
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) {
+      return false;
+    }
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) {
+      return undefined;
+    }
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) {
+      return false;
+    }
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
+//        10
+//     5     13
+//   2  7  11  16
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
 
-console.log(tree);
-console.log(tree.root.value); // 10
+// console.log(tree.root);
+console.log(tree.find(5));
